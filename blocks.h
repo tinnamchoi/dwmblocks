4448cc910cb1282dd6f4dbb81f2c344fb1aa4cad
tinnamchoi <tinnam.choi@gmail.com>
2022-06-30 06:48:21 +0930

    Calculate seconds since birth

--- blocks.h
+++ blocks.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/     /*Command*/                                                /*Update Interval*/ /*Update Signal*/
-	{"Mem:",     "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 7,                  0},
-	{"Bright:",  "cat /sys/class/backlight/amdgpu_bl0/brightness",          7,                  0},
-	{"Sound:",   "pamixer --get-volume",                                    7,                  0},
-	{"Battery:", "cat /sys/class/power_supply/BAT0/capacity",               7,                  0},
-	{"",         "date '+%Y%m%d-%H%M%S (%a)'",                              7,                  0},
+	{"Mem:",     "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 1,                  0},
+	{"Bright:",  "cat /sys/class/backlight/amdgpu_bl0/brightness",          0,                  2},
+	{"Sound:",   "pamixer --get-volume",                                    0,                  3},
+	{"Battery:", "cat /sys/class/power_supply/BAT0/capacity",               300,                0},
+	{"",         "date '+%Y%m%d-%H%M%S (%a)'",                              1,                  0},
+	{"",         "echo $(( `date +%s` - 1086267120 ))",                     1,                  0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
