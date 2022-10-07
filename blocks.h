6dac0e59c9766852ebecc6cb01233a3592f3becd
tinnamchoi <tinnam.choi@gmail.com>
2022-10-07 14:47:02 +1030

    Updated volume icon

--- blocks.h
+++ blocks.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/     /*Command*/                                                /*Update Interval*/ /*Update Signal*/
 	{" ",       "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 1,                  0},
 	{" ",       "cat /sys/class/backlight/*/brightness",                   0,                  2},
-	{" ",       "pamixer --get-volume",                                    0,                  3},
+	{"",         "volume-icon.sh",                                          0,                  3},
 	{" ",       "cat /sys/class/power_supply/BAT0/capacity",               300,                0},
 	{"",         "date '+%Y%m%d-%H%M%S (%a)'",                              1,                  0},
 	{"",         "echo $(( 3389291585 - `date +%s` ))",                     1,                  0},
