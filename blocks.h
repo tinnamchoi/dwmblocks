749d628b7f714b9e8b42c2f7091d274730bf95b7
tinnamchoi <tinnam.choi@gmail.com>
2022-10-07 14:18:29 +1030

    Style

--- blocks.h
+++ blocks.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/     /*Command*/                                                /*Update Interval*/ /*Update Signal*/
 	{" ",       "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 1,                  0},
-	{" ",       "cat /sys/class/backlight/*/brightness",          0,                  2},
+	{" ",       "cat /sys/class/backlight/*/brightness",                   0,                  2},
 	{" ",       "pamixer --get-volume",                                    0,                  3},
 	{" ",       "cat /sys/class/power_supply/BAT0/capacity",               300,                0},
 	{"",         "date '+%Y%m%d-%H%M%S (%a)'",                              1,                  0},
@@ -12,3 +12,4 @@ static const Block blocks[] = {
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
 static unsigned int delimLen = 3;
+
